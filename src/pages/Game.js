aad4a6c96da932cb7e1a65f921dc9d427430e1fa
Alexandar Karapenev <treildr@gmail.com>
2022-12-14 19:18:23 +0200

    Winner alert.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,6 +5,7 @@ import Board from '../components/Board'
 import Info from '../components/Info'
 import { getLocalUser } from '../localStore'
 import ReactConfetti from 'react-confetti'
+import { Alert, Button } from '@mui/material'
 
 export const Game = () => {
   const [board, setBoard] = useState([9, 9, 9, 9, 9, 9, 9, 9, 9])
@@ -17,7 +18,7 @@ export const Game = () => {
   const callback = useCallback(({ board, players, playerTurn, state, winner }) => {
     setBoard(board)
     setInfo({ players, playerTurn })
-    setFinished(state === "FINISHED")
+    setFinished(state === 'FINISHED')
     setWinner(winner)
     if (winner) setWinnerName(winner === players[0]?.id ? players[0]?.username : players[1]?.username)
   }, [])
@@ -32,11 +33,28 @@ export const Game = () => {
 
   return (
     <div className='App'>
-      {isFinished && winner != null && <ReactConfetti recycle={false} />} 
+      {isFinished && winner != null && <ReactConfetti recycle={false} />}
       <header>
         <div></div>
         <h1>Tic-Tac-Toe</h1>
+        <div className='button'>
+          {isFinished && (
+            <Button
+              variant='outlined'
+              onClick={() => {
+                navigate('/')
+              }}
+            >
+              Go back to 🏠
+            </Button>
+          )}
+        </div>
       </header>
+      <div className='finishedGame'>
+        <Alert style={{ visibility: isFinished ? 'visible' : 'hidden' }} severity={winner != null ? 'success' : 'info'}>
+          {winner == null ? 'The game is a Draw! 🎨' : `${winnerName} won! 🎉`}
+        </Alert>
+      </div>
       <div className='boardDisplay'>
         {board && (
           <Board
